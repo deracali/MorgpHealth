f8c46ae962d86c4eb7a7e1d16e26b98442f74bf7
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2024-11-15 01:57:01 -0800

    Update blogModel.js

--- Backend/models/blogModel.js
+++ Backend/models/blogModel.js
@@ -3,19 +3,15 @@ import mongoose from 'mongoose';
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
   },
   content: {
     type: String,
-    required: true,
   },
   author: {
     type: String,
-    required: true,
   },
   imageUrl: {
     type: String,
-    required: true,
   },
   date: {
     type: Date,
