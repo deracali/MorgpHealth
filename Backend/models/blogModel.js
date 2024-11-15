81d5a2ba83b7241c124e914c1453f96d61303b7a
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2024-11-15 01:24:10 -0800

    Update blogModel.js

--- Backend/models/blogModel.js
+++ Backend/models/blogModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const blogSchema = new mongoose.Schema({
   title: {
@@ -23,4 +23,4 @@ const blogSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Blog', blogSchema);
+export default mongoose.model('Blog', blogSchema);
