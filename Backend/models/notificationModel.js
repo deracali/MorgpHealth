4146ef704243dc108a0cbd4baa32d9d243058ec9
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2024-11-20 05:14:26 -0800

    Update notificationModel.js

--- Backend/models/notificationModel.js
+++ Backend/models/notificationModel.js
@@ -4,21 +4,17 @@ const notificationSchema = new mongoose.Schema(
   {
     recipientId: {
       type: mongoose.Schema.Types.ObjectId,
-      required: true,
       refPath: "recipientType",
     },
     recipientType: {
       type: String,
-      required: true,
       enum: ["User", "Doctor"], // Define valid recipient types
     },
     title: {
       type: String,
-      required: true,
     },
     message: {
       type: String,
-      required: true,
     },
     read: {
       type: Boolean,
